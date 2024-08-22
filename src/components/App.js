263272bf34024ba36f105126061806c159712f76
Evan Lee <evan.lee.615@gmail.com>
2024-08-22 01:56:47 -0400

    getting data set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 function App() {
+  const [items, setItemsState] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3002/items")
+    .then(response => response.json())
+    .then(data => setItemsState(data))
+  },[])
+
+  console.log(items)
 
   return (
     <div>
-
+      <img src={items[0].image} alt="alt" />
     </div>
   )
 }
