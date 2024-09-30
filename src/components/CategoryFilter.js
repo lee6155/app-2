27b145d0d004467e85ccad47c5ba85a3fe8a6dc3
Evan Lee <evan.lee.615@gmail.com>
2024-09-30 00:15:38 -0400

    refactored sort by category feature

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,23 +1,9 @@
 import React, { useState } from "react"
 
-function CategoryFilter({ passCategoryItems, items }) {
-    // const [forFilter, setForFilter] = useState([])
+function CategoryFilter({ passCategory }) {
 
-    // fetch("http://localhost:3000/items")
-    // .then(response => response.json())
-    // .then(data => setForFilter(data))
-         
     function handleChange(event) {
-        if(event.target.value !== "All") {
-            const categoryItems = items.filter(function(item){
-                return item.category === event.target.value                
-            })
-            return passCategoryItems(categoryItems)
-
-        } else {
-            const categoryItems = items
-            return passCategoryItems(categoryItems)
-        }   
+        return passCategory(event.target.value)   
     }
 
     return(
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,30 +5,34 @@ import NavBar from "../components/NavBar"
 
 function HomePage() {
     const [items, setItemsState] = useState([])
-    const [filtered, setFiltered] = useState([])
+    const [category, setCategory] = useState("All")
 
     useEffect(() => {
         fetch("http://localhost:3000/items")
         .then(response => response.json())
-        .then(function(data) {
-            setItemsState(data)
-            setFiltered(data)
-        })
+        .then(data => setItemsState(data))
     },[])
 
-    let displayItems = filtered.map(function(item){
+    let displayItems
+    category !== "All"?
+        displayItems = items.filter(function(item) {
+            return item.category === category
+        }) : 
+        displayItems = items
+    
+    let displayItems2 = displayItems.map(function(item) {
         return <ItemCard key={item.id} id={item.id} name={item.name} image={item.image} price={item.price}/>
     })
 
-    function passCategoryItems(newState) {
-        return setFiltered(newState)
+    function passCategory(newState) {
+        return setCategory(newState)
     }
 
     return(
         <div>
             <NavBar/>
-            <CategoryFilter items={items} passCategoryItems={passCategoryItems}/>
-            {displayItems}
+            <CategoryFilter passCategory={passCategory}/>
+            {displayItems2}
         </div>
     )
 }
